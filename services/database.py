699bf50cdcdab657ca33c5b78f2c7a5712218bc9
Maria Mourão <130841906+mariamourao@users.noreply.github.com>
2024-01-17 15:24:15 -0300

    Update database.py

--- services/database.py
+++ services/database.py
@@ -1,16 +1,7 @@
-# import pyodbc
-
-# server = 'seuservidor' 
-# database = 'seubancodedados' 
-# username = 'usuario' 
-# password = 'senha' 
-# cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-# cursor = cnxn.cursor()
-
 import psycopg2
 
-#def connect_to_postgresql():
-    # Substitua as informações abaixo com os detalhes do seu banco de dados PostgreSQL
+
+# Substitua as informações abaixo com os detalhes do seu banco de dados PostgreSQL
 host = 'localhost'  # ou o endereço IP do seu servidor PostgreSQL
 database = 'postgres'  # ou o nome do seu banco de dados
 user = 'postgres'
